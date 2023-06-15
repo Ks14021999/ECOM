d4982dc96a60b294d63fed924f09eb59287f69b3
ks14021999 <ks14021999@gmail.com>
2023-06-15 16:24:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// importing required packages
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const db = require('./config/mongoose');
+
+dotenv.config();
+
+// initializing express
+const app = express();
+
+// using body parser to parse over the request body
+app.use(bodyParser.urlencoded({extended: true}));
+
+// using routes
+app.use('/products', require('./routes/products'));
+
+// starting the server
+app.listen(3000, function(){
+    console.log('API is live on http://localhost:3000/products');
+});
